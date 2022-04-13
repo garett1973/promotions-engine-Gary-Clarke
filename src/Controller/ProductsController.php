bdf3a3944c68fda3ecb45ead93321d2ee01b7669
garett1973 <hobby@virgis.net>
2022-04-13 20:14:14 +0300

    added serializer

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,54 @@
 
 namespace App\Controller;
 
-class ProductsController
-{
+use App\DTO\LowestPriceEnquiry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+class ProductsController extends AbstractController
+{
+    #[Route('products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => "Promotions Engine failure message"],
+                $request->headers->get('force_fail')
+            );
+        }
+
+
+        // 1. Deserialize json data into a EnquiryDTO - Data Transfer Object
+
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+        dd($lowestPriceEnquiry);
+        // 2. Pass Enquiry into a promotions filter
+            // the appropriate promotion will be applied
+        // 3. Return modified Enquiry, serialize it back into Json and send Json response
+
+        return new JsonResponse([
+            "quantity" => 4,
+            "request_location" => "LT",
+            "voucher_code" => "LT76183",
+            "request_date" => "2022-04-13",
+            "product_id" => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], 200);
+
+
+    }
+
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+
+    }
 }
