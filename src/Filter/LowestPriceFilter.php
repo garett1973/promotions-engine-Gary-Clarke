a7720fcfeec240b8279bbf37146eb71dd19d7258
garett1973 <hobby@virgis.net>
2022-05-13 16:16:50 +0300

    productPromotions - part 12 finished

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -10,7 +10,23 @@ class LowestPriceFilter implements PromotionsFilterInterface
 
     public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
     {
-        $enquiry->setDiscountedPrice(50);
+
+        $price = $enquiry->getProduct()->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $price * $quantity;
+        // Loop over the promotions
+        // Run the promotions modification logic against the enquiry
+        // 1. check does the promotion apply e.g. is it in date range/ is the voucher code valid?
+        // 2. apply the price modification to obtain a $modifiedPrice (how?)
+
+//        $modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
+
+        // 3. check if the modified price is lower than the current lowest price $lowestPrice
+            // 1. Save to Enquiry properties
+            // 2. Update the lowest price $lowestPrice
+
+
+        $enquiry->setDiscountedPrice(250);
         $enquiry->setPrice(100);
         $enquiry->setPromotionId(3);
         $enquiry->setPromotionName('Black Friday half price sale');
